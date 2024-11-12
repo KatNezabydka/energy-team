63bf50c7ec977f40c686b7431a352344edccb429
KatNezabydka <katbackdev@gmail.com>
2024-11-12 07:53:56 +0000

    Deploying to gh-pages from @ KatNezabydka/energy-team@d57c2f3a17d2128fa9085d470f67a61f70d95c03 🚀

---
+++ assets/favorites-BOrhoyXY.js
@@ -0,0 +1,2 @@
+import{c as t}from"./mob-menu-BPOB6Qu1.js";import"./vendor-BT7OT44q.js";let r=JSON.parse(localStorage.getItem("favorites_exercises_ls_key"))||[];function s(){const e=JSON.parse(localStorage.getItem("favorites_exercises_ls_key"))||[];JSON.stringify(e)!==JSON.stringify(r)&&(r=e,t())}setInterval(s,2e3);
+//# sourceMappingURL=favorites-BOrhoyXY.js.map
